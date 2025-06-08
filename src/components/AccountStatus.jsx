6b01089bce661694fe86bfd9f1b1a16f6bbd583a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-08 17:34:38 +0100

    feat: initial component render

---
+++ src/components/AccountStatus.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  
+`
+
+const AccountStatus = () => {
+  return (
+    <Container>
+      logged in name and logout button
+    </Container>
+  );
+};
+
+export default AccountStatus;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  color: blueviolet;
+  font-size: 1.5rem;
+  font-weight: bold;
+`
+
+const Logo = () => {
+  return (
+    <Container>
+      Messenger
+    </Container>
+  );
+};
+
+export default Logo;
