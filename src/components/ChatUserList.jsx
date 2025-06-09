54a93a11d79e6b3bba99fd69ee0680fb2b2871fa
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-09 19:05:08 +0100

    feat: initial chat user list

---
+++ src/components/ChatUserList.jsx
@@ -0,0 +1,32 @@
+import { ChevronUp } from "lucide-react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
+
+const IconWrapper = styled.div`
+  transition: transform 0.3s ease;
+  transform: ${({ $expanded }) => ($expanded ? "rotate(180deg)" : "rotate(0deg)")};  
+`
+
+const ChatUserList = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div>
+      <Header onClick={() => setExpanded(!expanded)}>
+        <h1>Users</h1>
+        <IconWrapper $expanded={expanded} >
+          <ChevronUp size={30} />
+        </IconWrapper>
+      </Header>
+    </div>
+  );
+};
+
+export default ChatUserList;
