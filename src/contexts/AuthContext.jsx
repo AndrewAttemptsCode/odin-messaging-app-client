336a8d45d4ae1e688d9916c86aa533540ec802c7
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-11 19:11:48 +0100

    feat: add auth context based on jwt token expirary checks

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { jwtDecode } from "jwt-decode";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
