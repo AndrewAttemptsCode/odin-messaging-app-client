e1582e01f199899d2b4ba72623dcd20dde0f6e4b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-12 17:35:02 +0100

    feat: add display of current logged in username and logout button

--- src/components/AccountStatus.jsx
+++ src/components/AccountStatus.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { AuthContext } from "../contexts/AuthContext";
+import capitalize from "../utils/capitalize";
 
 const Container = styled.div`
-  
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
 `
 
 const AccountStatus = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <Container>
-      logged in name and logout button
+      {capitalize(user.username)}
+      <button onClick={() => logout()}>Logout</button>
     </Container>
   );
 };
