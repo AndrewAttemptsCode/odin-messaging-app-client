dc3596d92c1f610399e0b5a215a6c6680573af60
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-12 17:34:40 +0100

    feat: add auto logout timer and setting the user with id and username

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,32 +1,57 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({ id: 0, username: "" });
+  const [loading, setLoading] = useState(true);
 
-  const isAuthorized = () => {
+  useEffect(() => {
     const storedToken = localStorage.getItem("token");
+
+    if (!storedToken) {
+      setLoading(false);
+      return;
+    }
+
     if (storedToken) {
       try {
-        const { exp } = jwtDecode(storedToken);
+        const { id, username, exp } = jwtDecode(storedToken);
         const now = Date.now() / 1000;
         if (exp < now) {
           logout();
         }
+        
+        setUser({ id, username });
+
+        const timeUntilExpiry = (exp - now) * 1000;
+        const logoutTimeout = setTimeout(() => {
+          logout();
+        }, timeUntilExpiry);
+
+        return () => clearTimeout(logoutTimeout);
+
       } catch (error) {
         console.error("Jwt decode error:", error);
+      } finally {
+        setLoading(false);
       }
     }
-  }
+  }, []);
+
+  const isAuthorized = user.id !== 0;
 
   const logout = () => {
     localStorage.removeItem("token");
+    setUser({ id: 0, username: "" });
   }
 
   const values = {
     isAuthorized,
     logout,
+    user,
+    loading,
   }
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
