c8ce41702c419468105447284a1a39728b73221d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-14 14:40:12 +0100

    feat: initial chat context to connect chat with two users

---
+++ src/contexts/ChatContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [activeChat, setActiveChat] = useState({ sender: null, receiver: null });
+  
+  const connectChat = async (senderId, receiverId) => {
+    setActiveChat({ sender: senderId, receiver: receiverId });
+    console.log("Active Chat:", activeChat);
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/chat/${senderId}/${receiverId}`, {
+        method: "get",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        }
+      })
+
+      const data = await response.json();
+      console.log(data);
+
+    } catch(err) {
+      console.error("Error fetching chat", err);
+    }
+  }
+  
+  const values = {
+    connectChat,
+  }
+
+  return (
+    <ChatContext.Provider value={values}>{children}</ChatContext.Provider>
+  );
+};
