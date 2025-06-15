a008d3ee2ba12d0c9c115fd3ffd4b8d47431c4b5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-15 18:37:20 +0100

    feat: add auto scroll to the bottom of the chat window if chat scroll overflows

--- src/components/ChatConvo.jsx
+++ src/components/ChatConvo.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { ChatContext } from "../contexts/ChatContext";
 
@@ -10,9 +10,16 @@ const Container = styled.div`
 
 const ChatConvo = () => {
   const { chatMessages } = useContext(ChatContext);
+  const chatRef = useRef(null);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [chatMessages]);
 
   return (
-    <Container>
+    <Container ref={chatRef}>
       {chatMessages && chatMessages.map((message) => (
         <p key={message.id}>{message.sender.username} - {message.createdAt} - {message.text}</p>
       ))}
