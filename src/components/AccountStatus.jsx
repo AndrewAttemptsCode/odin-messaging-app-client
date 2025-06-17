43158203152e6490d7ae5641c226c5641cf1c377
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-17 17:58:08 +0100

    feat: add drop down menu to access settings and logout

--- src/components/AccountStatus.jsx
+++ src/components/AccountStatus.jsx
@@ -1,21 +1,60 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import { AuthContext } from "../contexts/AuthContext";
 import capitalize from "../utils/capitalize";
+import { ChevronDown, LogOut, Settings } from "lucide-react";
 
 const Container = styled.div`
+  position: relative;
   display: flex;
   gap: 0.5rem;
   align-items: center;
+  padding: 0.5rem 1rem;
+`
+
+const StatusWrapper = styled.div`
+  display: flex;
+`
+
+const DropMenu = styled.nav`
+  position: absolute;
+  top: 115%;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: red;
+  padding: 0.5rem;
+  gap: 0.5rem;
+`
+
+const MenuItem = styled.div`
+  display: flex;
+  gap: 0.5rem;
 `
 
 const AccountStatus = () => {
   const { user, logout } = useContext(AuthContext);
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <Container>
-      {capitalize(user.username)}
-      <button onClick={() => logout()}>Logout</button>
+    <Container onClick={() => setExpanded(!expanded)}>
+      <StatusWrapper>
+        {capitalize(user.username)}
+        <ChevronDown />
+      </StatusWrapper>
+      {expanded && 
+        <DropMenu>
+          <MenuItem>
+            <Settings />
+            <p>Settings</p>
+          </MenuItem>
+          <MenuItem>
+            <LogOut />
+            <p onClick={() => logout()}>Logout</p>
+          </MenuItem>
+        </DropMenu>
+      }
     </Container>
   );
 };
