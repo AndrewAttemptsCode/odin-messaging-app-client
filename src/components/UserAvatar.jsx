534b6e8abeb26a95f7b2a6e252554b3d76970f33
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-18 16:33:53 +0100

    feat: add default props for styling

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,24 +1,26 @@
 import styled from "styled-components";
+import capitalizeInitial from "../utils/capitalizeInitial";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 40px;
-  width: 40px;
+  height: ${({$size}) => `${$size}px`};
+  width: ${({$size}) => `${$size}px`};
   border-radius: 50%;
-  color: white;
+  color: #ffffff;
   border: none;
   user-select: none;
   font-weight: bold;
   padding: 4px;
-  background-color: cadetblue;
+  background-color: ${({$bg}) => $bg};
 `
 
-const UserAvatar = () => {
+const UserAvatar = ({ bg, size=40, username="?" }) => {
+  const bgColor = bg || "#5f9ea0";
   return (
-    <Container>
-      A
+    <Container $bg={bgColor} $size={size}>
+      {capitalizeInitial(username)}
     </Container>
   );
 };
