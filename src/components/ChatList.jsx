8c66e386bb1a62dc7d2f9ead6386498d69b8dfe8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-18 16:38:02 +0100

    feat: add user avatar that passes the avatar color

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,9 +1,24 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { ChatContext } from "../contexts/ChatContext";
+import UserAvatar from "./UserAvatar";
+import styled from "styled-components";
+import capitalize from "../utils/capitalize";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 0.5rem;
+`
 
 const ChatList = () => {
-  const [chatUsers, setChatUsers] = useState([{ id: 0, username: "" }]);
+  const [chatUsers, setChatUsers] = useState([{ id: 0, username: "", avatarColor: "", usernameColor: "" }]);
   const [loading, setLoading] = useState(false);
   const { user } = useContext(AuthContext);
   const { connectChat } = useContext(ChatContext);
@@ -36,13 +51,16 @@ const ChatList = () => {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <>
+    <Container>
       {chatUsers
       .filter(chatUser => chatUser.id !== user.id)
       .map(chatUser => (
-        <div key={chatUser.id} onClick={() => handleOnClick(chatUser.id)}>{chatUser.username}</div>
+        <UserContainer key={chatUser.id} onClick={() => handleOnClick(chatUser.id)}>
+          <UserAvatar bg={chatUser.avatarColor} size={40} username={chatUser.username} />
+          {capitalize(chatUser.username)}
+        </UserContainer>
       ))}
-    </>
+    </Container>
   )
 }
 
