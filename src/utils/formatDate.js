fff99afcbecd71401b976fbdd26600e7e8c33120
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-19 18:03:00 +0100

    feat: add helper function to format dates

---
+++ src/utils/formatDate.js
@@ -0,0 +1,10 @@
+const formatDate = (date) => {
+  const dateObj = new Date(date);
+  const day = String(dateObj.getDate()).padStart(2, 0);
+  const month = String(dateObj.getMonth() + 1).padStart(2, 0);
+  const year = String(dateObj.getFullYear()).slice(-2);
+  
+  return `${day}/${month}/${year}`;
+}
+
+export default formatDate;
