514e102c6cabe9e8a935d73a50179e3cf7dd476c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-19 18:23:43 +0100

    feat: add convo message with user's avatar, info section and message

---
+++ src/components/ChatConvoMessage.jsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import UserAvatar from "./UserAvatar";
+import formatDate from "../utils/formatDate";
+import formatTime from "../utils/formatTime";
+import capitalize from "../utils/capitalize";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 1rem;
+  padding: 1rem;
+  border: 1px solid red;
+`
+
+const AvatarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const InfoContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const MessageContainer = styled.div`
+  display: flex;
+  white-space: pre-wrap;
+`
+
+const ChatConvoMessage = ({ username, createdAt, message, avatarColor, usernameColor }) => {
+  return (
+    <Container>
+      <AvatarWrapper>
+        <UserAvatar
+          bg={avatarColor}
+          username={username}
+          size={60}
+        />
+      </AvatarWrapper>
+      <BodyContainer>
+        <InfoContainer>
+          <p>{capitalize(username)}</p>
+          <p>{formatDate(createdAt)}</p>
+          <p>{formatTime(createdAt)}</p>
+        </InfoContainer>
+        <MessageContainer>
+          <p>{message}</p>
+        </MessageContainer>
+      </BodyContainer>
+    </Container>
+  );
+};
+
+export default ChatConvoMessage;
