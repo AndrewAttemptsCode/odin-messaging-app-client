12ce61160e0d3c7103062cc19061587b8e3de1e8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-24 16:12:31 +0100

    feat: only show user list on the chat page with use of uselocation and hide the button on viewports bigger than 770px wide

--- src/components/ChatMenuButton.jsx
+++ src/components/ChatMenuButton.jsx
@@ -2,15 +2,22 @@ import { useContext } from "react";
 import { ChatContext } from "../contexts/ChatContext";
 import styled from "styled-components";
 import { Menu } from "lucide-react";
+import { useLocation } from "react-router-dom";
 
 const StyledMenu = styled(Menu)`
   cursor: pointer;
+
+  @media (min-width: 770px) {
+    display: none;
+  }
 `
 
 const ChatMenuButton = () => {
   const { menuOpen, setMenuOpen } = useContext(ChatContext);
+  const location = useLocation();
 
   return (
+    location.pathname === "/" &&
     <StyledMenu
       onClick={() => setMenuOpen(!menuOpen)}
     />
