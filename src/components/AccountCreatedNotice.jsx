8ce28bdb2fe345896ac174a068cd90f49bfef0ec
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-26 18:51:34 +0100

    styles: add styling and responsive design

--- src/components/AccountCreatedNotice.jsx
+++ src/components/AccountCreatedNotice.jsx
@@ -6,17 +6,30 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-width: fit-content;
+  width: 90%;
+  padding: max(2%, 1rem);
+  color: #333333;
+  background-color: #F5F5F5;
+  border-radius: 10px;
+  box-shadow: 0 0 5px 5px #9370DB;
+
+  h1 {
+    color: #191970;
+  }
+
+  a {
+    font-size: 1.1rem;
+    font-weight: bold;
+  }
 `
 
 const AccountCreatedNotice = ({ user }) => {
   return (
     <Container>
-      <h1>{capitalize(user.username)}!</h1>
-      <p>Your account has been created</p>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/login"}>Login</Link>
-      </div>
+      <h1>Welcome, {capitalize(user.username)}.</h1>
+      <p>Your account has been created!</p>
+      <Link to={"/login"}>Login</Link>
     </Container>
   );
 };
